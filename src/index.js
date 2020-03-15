c0e805580a91ea808944528b50138059000f5a8e
Nikitas Nikolaev <troll-men@mail.ru>
2020-03-15 15:43:04 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //return optionOne(str,bracketsConfig);
+  let oldLength;
+  let newLength;
+
+  do {
+    oldLength = str.length;
+
+    for(let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1], '');
+    }
+
+    newLength = str.length;
+
+  } while(oldLength != newLength)    
+
+  return str.length > 0 ? false : true;
 }
